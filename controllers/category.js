ffd49e33195fc810d0bee74fc23cf6897cb85449
Briyan <callmebantu148@gmail.com>
2021-04-07 17:41:37 +0530

    till different categories

---
+++ controllers/category.js
@@ -0,0 +1,40 @@
+const Category = require("../models/Category");
+
+exports.getCategoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "category is not found",
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
+exports.createCategory = (req, res) => {
+  const category = new Category(req.body);
+  category.save((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "not able to save category in BD",
+      });
+    }
+    res.json({ category });
+  });
+};
+
+exports.getCategory = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.getAllCategory = (req, res) => {
+  Category.find().exec((err, categories) => {
+    if (err) {
+      return res.status(400).json({
+        error: "no Categories",
+      });
+    }
+    res.json(categories);
+  });
+};
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const {
   getUsers,
   getUser,
   updateUser,
+  userPurchaseList,
 } = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
@@ -13,4 +14,10 @@ router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get(
+  "/orders/user/:userId",
+  isSignedIn,
+  isAuthenticated,
+  userPurchaseList
+);
 module.exports = router;
